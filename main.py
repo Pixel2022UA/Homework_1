db21e96492f18efa5969b7ee2548648c0992a518
Pixel2022UA <zelenskiy2013@gmail.com>
2023-04-11 13:40:11 +0300

    Commit func parse_cookie

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    my_dict = {}
+    if query:
+        for elem in query.split(';'):
+            key_value = elem.split('=', maxsplit=1)
+            if len(key_value) == 2:
+                key, value = key_value
+                value_parts = value.split('=')
+                if len(value_parts) > 1:
+                    value = '='.join(value_parts)
+                my_dict[key] = value
+    return my_dict
 
 
 if __name__ == '__main__':
@@ -19,3 +29,9 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('name=Dima;name=Vasya;') == {'name': 'Vasya'}
+    assert parse_cookie('name=Dima;name=Vasya;name=Kolya;') == {'name': 'Kolya'}
+    assert parse_cookie('name=Dima;age=28;city=Odessa') == {'name': 'Dima', 'age': '28', 'city': 'Odessa'}
+    assert parse_cookie('name=Dima=User=Test;age=28;') == {'name': 'Dima=User=Test', 'age': '28'}
+    assert parse_cookie('name=Dima;age=28;city=Kyiv;') == {'name': 'Dima', 'age': '28', 'city': 'Kyiv'}
+    assert parse_cookie('name=Dima;age=15;city=Paris;city=Berlin;') == {'name': 'Dima', 'age': '15', 'city': 'Berlin'}
